2ab8e9ed14542b553d2c56e99a59dc316db7da5a
said_sadu <sadullaev.1996@gmail.com>
2022-07-17 14:29:59 +0300

    решена первая задача

---
+++ ex001/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int number = new Random().Next(10, 100);
+//Console.WriteLine("number: {number}");
+Console.WriteLine($"number: {number}");
+int x = number / 10;
+int y = number % 10;
+
+if (x>y)
+{
+    Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine(y);
+}
