0a958a1b003be39a1e923b56165de131b3484413
said_sadu <sadullaev.1996@gmail.com>
2022-07-25 10:41:15 +0300

    решение 2 (13)задачи

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numText = Convert.ToString(num);
+if (numText.Length > 2)
+{
+  Console.WriteLine("это третья цифра -> " + numText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
